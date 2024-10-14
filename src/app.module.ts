ad67a1cbf7ead813db1a1306fa60fba03d75a3dc
tohir <tohirdavrsmart@gmail.com>
2024-10-14 13:40:51 +0500

    fx

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthController } from './src/auth/auth.controller';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthController } from './auth/auth.controller'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [AuthModule],
